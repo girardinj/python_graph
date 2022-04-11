e9094f9cc8798db9bef2cc97d3eee46e8b3ada34
Jarod Girardin <jarod.girardin@he-arc.ch>
2022-04-11 13:02:53 +0200

    better explanation

--- example1.py
+++ example1.py
@@ -1,4 +1,5 @@
 from graph import Graph
+from graph.display_graph import print_console_path, print_graph
 
 graph = Graph()
 
@@ -7,7 +8,8 @@ v1 = graph.add_value_as_vertex(1)
 v2 = graph.add_value_as_vertex(2)
 v3 = graph.add_value_as_vertex(3)
 
-graph.connect_two_vertex(v0, v1, "salut")
-graph.connect_two_vertex(v1, v3, "Bonjour")
+graph.connect_two_vertex(v0, v1, "from v0 to v1")
+graph.connect_two_vertex(v1, v3, "from v1 to v3")
 
 print(graph)
+print_graph(graph, [], None, None, ratio=70, format='pdf', name='my graph')
